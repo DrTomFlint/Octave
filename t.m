218875e6ef9ca831a3051db318ca0a8888c6e567
Dr Tom Flint <drtomflint@gmail.com>
2020-03-06 16:52:14 -0700

    add t.m

--- t.m
+++ t.m
@@ -2,7 +2,15 @@
 #
 # this is a test script to call new functions
 
-s = bopen(1);
+for r=[0:5:255],
+  for g=[0:5:255],
+    for b=[0:5:255],
+      printf("color %3d %3d %3d\n",r,g,b);
+      bcolor(s,r,g,b);
+      pause(0.5);
+    endfor
+  endfor
+endfor
 pause(3);
 err = bmode(s,2);
 
